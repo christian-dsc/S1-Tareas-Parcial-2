d428b283e13c32383550ba5707e3845cda611018
cristiano-dsc <csosa.edu@gmail.com>
2026-08-12 18:26:13 -0500

    mi primer programa

---
+++ Christian_Sosa_Tarea9.py
@@ -0,0 +1,10 @@
+# Tarea Semana 9 - Arreglos en Python
+#! Estudiante: Christian Sosa
+
+arreglo = [10, 20, 30, 40, 50]
+
+print("Valor en la posición 1:", arreglo[0])
+
+print("Recorrido del arreglo:")
+for numero in arreglo:
+    print(numero)
